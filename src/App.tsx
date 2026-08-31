f341c2984c57d8f59e28596425979d309811274f
jhonne jefferson morais lima junior <felipefelipejulio242@gmail.com>
2026-08-31 14:59:25 -0300

    Cria o card de aluno com props tipadas e situacao de aprovacao (Etapa 0)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Cabecalho from "./components/Cabecalho";
+import AlunoCard from "./components/AlunoCard";
+import { alunosIniciais } from "./mock";
 
 function App() {
+  // provisório: no Desafio 1 os dados passam a vir do api.ts
   return (
     <div className="pagina">
       <Cabecalho />
+      <AlunoCard aluno={alunosIniciais[0]} />
     </div>
   );
 }
---
+++ src/components/AlunoCard.tsx
@@ -0,0 +1,38 @@
+import type { Aluno } from "../types";
+
+interface AlunoCardProps {
+  aluno: Aluno;
+}
+
+function AlunoCard({ aluno }: AlunoCardProps) {
+  const aprovado = aluno.media >= 6;
+  const classeSelo = aprovado ? "selo selo-aprovado" : "selo selo-reprovado";
+  const textoSelo = aprovado ? "Aprovado" : "Reprovado";
+
+  return (
+    <article className="card-aluno">
+      <div className="card-topo">
+        <h3>{aluno.nome}</h3>
+        <span className={classeSelo}>{textoSelo}</span>
+      </div>
+      <dl className="card-dados">
+        <div>
+          <dt>Matrícula</dt>
+          <dd>{aluno.matricula}</dd>
+        </div>
+        <div>
+          <dt>Idade</dt>
+          <dd>{aluno.idade} anos</dd>
+        </div>
+      </dl>
+      <div className="card-rodape">
+        <div className="media-bloco">
+          <span className="media-valor">{aluno.media}</span>
+          <span className="media-rotulo">média</span>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default AlunoCard;
