be7f7e94d28aaa957fc8d83f7ad26efb213ce2bc
jhonne jefferson morais lima junior <felipefelipejulio242@gmail.com>
2026-08-31 14:58:31 -0300

    Cria o cabecalho e remove a tela provisoria (Etapa 0)

---
+++ src/components/Cabecalho.tsx
@@ -0,0 +1,23 @@
+function Cabecalho() {
+  return (
+    <header className="cabecalho">
+      <h1>
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"></path>
+          <path d="M22 10v6"></path>
+          <path d="M6 12.5V16a6 3 0 0 0 12 0v-3.5"></path>
+        </svg>
+        <span className="somente-leitura">Portal de Gestão Escolar</span>
+      </h1>
+    </header>
+  );
+}
+
+export default Cabecalho;
